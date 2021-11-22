0bff2f00cf33b42136ca866996718eefead08c8c
Jean-Philippe Steinmetz <jean-philippe@acceleratxr.com>
2021-11-22 09:37:25 -0800

    Adding triplet override

---
+++ triplets/x64-linux.cmake
@@ -0,0 +1,6 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(ENV{CC} /usr/bin/clang-9)
+set(ENV{CXX} /usr/bin/clang++-9)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
